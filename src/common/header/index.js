ee2fd07ea4b4b2721407732fb3344befe7cdedb9
yuanlijian <yuanlijianvip@163.com>
2020-05-17 06:10:55 +0800

    引入redux react-redux 使用combineReducers

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+
 
 import {
     HeaderWrapper,
@@ -12,65 +14,64 @@ import {
     Button
 } from './style';
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
+const Header = (props) => {
+    return (
+        <HeaderWrapper>
+            <Logo />
+            <Nav>
+                <NavItem className="left active">首页</NavItem>
+                <NavItem className="left">下载App</NavItem>
+                <NavItem className="right">登录</NavItem>
+                <NavItem className="right">
+                    <i className="iconfont">&#xe636;</i>
+                </NavItem>
+                <SearchWrapper>
+                    <CSSTransition
+                        in={props.focused}
+                        timeout={200}
+                        classNames='slide'
+                    >
+                        <NeaSearch
+                            className={props.focused ? 'focused' : ''}
+                            onFocus={props.handleInputFocus}
+                            onBlur={props.handleInputBlur}
+                        ></NeaSearch>
+                    </CSSTransition>
+                    <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe637;</i>
+                </SearchWrapper>
+                <Addition>
+                    <Button className="writting">
+                        <i className="iconfont">&#xe6e5;</i>
+                        写文章
+                    </Button>
+                    <Button className="reg">注册</Button>
+                </Addition>
+            </Nav>
+        </HeaderWrapper>
+    )
+}
 
-        this.state = {
-            focused: false
-        }
-
-        this.handleInputFocus = this.handleInputFocus.bind(this);
-        this.handleInputBlur = this.handleInputBlur.bind(this);
-    }
-
-    render() {
-        return (
-            <HeaderWrapper>
-                <Logo />
-                <Nav>
-                    <NavItem className="left active">首页</NavItem>
-                    <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">登录</NavItem>
-                    <NavItem className="right">
-                        <i className="iconfont">&#xe636;</i>
-                    </NavItem>
-                    <SearchWrapper>
-                        <CSSTransition
-                            in={this.state.focused}
-                            timeout={200}
-                            classNames='slide'
-                        >
-                            <NeaSearch
-                                className={this.state.focused ? 'focused' : ''}
-                                onFocus={this.handleInputFocus}
-                                onBlur={this.handleInputBlur}
-                            ></NeaSearch>
-                        </CSSTransition>
-                        <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe637;</i>
-                    </SearchWrapper>
-                    <Addition>
-                        <Button className="writting">
-                            <i className="iconfont">&#xe6e5;</i>
-                            写文章
-                        </Button>
-                        <Button className="reg">注册</Button>
-                    </Addition>
-                </Nav>
-            </HeaderWrapper>
-        )
-    }
-
-    handleInputFocus() {
-        this.setState({
-            focused: true
-        })
-    }
-    handleInputBlur() {
-        this.setState({
-            focused: false
-        })
+const mapStateToProps = (state) => {
+    return {
+        focused: state.header.focused
     }
 }
 
-export default Header;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleInputFocus() {
+            const action = {
+                type: 'search_focus'
+            };
+            dispatch(action);
+        },
+        handleInputBlur() {
+            const action = {
+                type: 'search_blur'
+            };
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
