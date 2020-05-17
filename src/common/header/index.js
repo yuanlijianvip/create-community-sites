8cce10d7044f349abb370483fbbad9853bebf9a8
yuanlijian <yuanlijianvip@163.com>
2020-05-17 09:00:00 +0800

    使用immutable.js redux-immutabel

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
-
+import { actionCreators }  from './store';
 
 import {
     HeaderWrapper,
@@ -53,23 +53,18 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.focused
+        focused: state.getIn(['header', 'focused'])
+        // focused: state.get('header').get('focused')
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
         handleInputFocus() {
-            const action = {
-                type: 'search_focus'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchFocus());
         },
         handleInputBlur() {
-            const action = {
-                type: 'search_blur'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchBlur());
         }
     }
 }
