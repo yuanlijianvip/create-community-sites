476197e2c42b4bdab5fa622b16d545e7af47e7c1
yuanlijian <yuanlijianvip@163.com>
2020-05-17 11:56:46 +0800

    代码优化

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -8,16 +8,27 @@ const defaultState = fromJS({
 })
 
 export default (state = defaultState, action) => {
-    if (action.type === constants.SEARCH_FOCUS) {
-        //immutable对象的值得set方法，会结合之前immutable对象的值
-        //和设置的值，返回一个全新的对象
-        return state.set('focused', true);
+    switch(action.type) {
+        case constants.SEARCH_FOCUS:
+            return state.set('focused', true);
+        case constants.SEARCH_BLUR:
+            return state.set('focused', false);
+        case constants.CHANGE_LIST:
+            return state.set('list', action.data);
+        default:
+            return state;
     }
-    if (action.type === constants.SEARCH_BLUR) {
-        return state.set('focused', false);
-    }
-    if (action.type === constants.CHANGE_LIST) {
-        return state.set('list', action.data);
-    }
-    return state;
+
+    // if (action.type === constants.SEARCH_FOCUS) {
+    //     //immutable对象的值得set方法，会结合之前immutable对象的值
+    //     //和设置的值，返回一个全新的对象
+    //     return state.set('focused', true);
+    // }
+    // if (action.type === constants.SEARCH_BLUR) {
+    //     return state.set('focused', false);
+    // }
+    // if (action.type === constants.CHANGE_LIST) {
+    //     return state.set('list', action.data);
+    // }
+    // return state;
 }
