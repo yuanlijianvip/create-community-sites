48b0fb029fe2552fc5f157328c6b480b58bc4aeb
yuanlijian <yuanlijianvip@163.com>
2020-05-19 22:54:35 +0800

    首页专题

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { TopicWrapper, TopicItem } from '../style';
 
 class Topic extends Component {
   render() {
+    const { list } = this.props;
     return (
       <div>
-        <div>Topic~</div>
+        <TopicWrapper>
+          {
+            list.map((item) => {
+              return (
+                <TopicItem key={item.get('id')}>
+                  <img 
+                    className='topic-pic'
+                    src={item.get('imgUrl')}
+                  />
+                  {item.get('title')}
+                </TopicItem>
+              )
+            })
+          }
+        </TopicWrapper>
       </div>
     );
   }
 }
 
-export default Topic;
+const mapState = (state) => ({
+  list: state.get('home').get('topicList')
+});
+
+export default connect(mapState, null)(Topic);
